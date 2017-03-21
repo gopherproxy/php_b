00925e93dbe1363c3e54b469ca7d172ec6f276fe
Marc <gopherproxy@gmail.com>
2017-03-21 12:16:11 +0100

    conditions.php
    opgave med study points

---
+++ conditions.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>If betingelse</title>
+</head>
+
+<body>
+<!-- Output afhængig af antallet af study points-->
+<!-- Når study points er under 80 skal der vises en rød tekst med advarsel: 'Kun [antallet af study points] er ikke nok! -->
+<!-- Når study points er lige med eller over 80 en grøn tekst: Super, du kan går op til din eksamen-->
+<!-- Når study points er over 100: Du har formodentlig snydt -->
+<!-- NO INLINE STYLES PLEAAAASE !-->
+</body>
+</html>

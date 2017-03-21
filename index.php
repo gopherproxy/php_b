ed4cd345503b4d1ea32af0ade6e39c2e0618ce05
gopherproxy <gopherproxy@gmail.com>
2017-03-21 09:47:13 +0100

    echo vs print

--- index.php
+++ index.php
@@ -8,7 +8,11 @@
 <body>
 <?php 
 // start af en php blok
-	echo "Hello World!";
+// echo viser data som skærm-output og kan have mere end en "expression"
+	echo "<h1>Hello World!</h1>", 4, 5, 'bla';
+// en alternativ er print metoden. Echo er dog lidt hurtigere
+// en datatype string kan (ligesom i JS) markeres med " eller '
+	print 'Jeg hedder Marc Vader!';
 // slut af en php blok:
 ?>
 </body>

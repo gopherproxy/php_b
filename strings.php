43d13f4e046f0ef674358c543b84cf97b225d8c6
gopherproxy <gopherproxy@gmail.com>
2017-03-21 11:18:36 +0100

    conditions

--- strings.php
+++ strings.php
@@ -6,7 +6,7 @@ $myVar = 'Marc';
 $myVar2 = 3;
 // i PHP er \ den såkaldte escape-character
 $book = 'The Hitchhiker\'s Guide to the Galaxy';
-$author = 'Douglas Adams';
+$author = 'Dan Brown';
 $book1 = "The Hitchhiker's Guide to the Galaxy";
 // som udgangspunkt brug kun "single quotes" i string formatering pga HTML konflikter
 echo '<h1 class="red">My Headline</h1>';
@@ -26,7 +26,30 @@ echo '<h1 class="red">My Headline</h1>';
 	echo $myVar;
 	echo '<br>';
 	// concatenation (konkatenering) med . konkatenerings operatør
-	echo '<h2>' . $book . ' by ' . $author . '</h2>';
+	// konkatenering kan kombinere variabler og strings
+	// en "conditional statement" med tredoppelt lighedstegn tjekker for overenstemmelse i indhold + datatype!
+	
+	/* som h2: 
+		Hvis author er Lars Løkke: Lars Løkke er dum
+		Hvis det er en tredje author:
+		Author er hverken Douglas Adams eller Lars Løkke, men [indhold af variablen $author]
+	*/
+	if($author === 'Douglas Adams'){
+		echo '<h2>' . $book . ' by ' . $author . '</h2>';
+	} else if($author === 'Lars Løkke'){
+		echo '<h2>' . $author . ' er dum.' . '</h2>';
+	} else {
+		echo '<h2>' . 'Author er ikke Douglas Adams, men ' . $author . '</h2>';
+	}
+	
 ?>
 </body>
 </html>
+
+
+
+
+
+
+
+

e45139db765037a0e7af74351a32edfb6e1a13ac
Tue Becher <tue@Tues-Mac.local>
2017-03-22 15:34:41 +0100

    Add & Sub implemented

--- calc.php
+++ calc.php
@@ -7,18 +7,52 @@
 
 <body>
 
+<?php
+	
+	function addNumbers($num1, $num2){
+		return $num1+$num2;
+	}
+
+	function subNumbers($num1, $num2){
+		return $num1-$num2;
+	}
+	
+	
+
+	$a = filter_input(INPUT_GET, 'a');
+	$b = filter_input(INPUT_GET, 'b');
+	$cmd = filter_input(INPUT_GET, 'cmd');
+	
+//	echo 'a='.$a.' b='.$b.' cmd='.$cmd;
+//	echo 'add:'.(addNumbers($a,$b)-subNumbers($a,$a));
+	
+	switch($cmd) {
+			
+		case 'Add':
+			$res = addNumbers($a,$b);
+			break;
+		case 'Sub':
+			$res = subNumbers($a,$b);
+			break;
+		default:
+			$res = 'Unknown cmd: '.$cmd;
+	}
+	
+?>
+
+
 <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
 
 	<label for="a">A:</label>
-	<input id="a" type="number" name="a"><br>
+	<input id="a" type="number" name="a" value="<?=$a?>"><br>
 	<label for="b">B:</label>
-	<input id="b" type="number" name="b"><br>
+	<input id="b" type="number" name="b" value="<?=$b?>"><br>
 	<hr>
 	<input type="submit" name="cmd" value="Add">
 	<input type="submit" name="cmd" value="Sub">
 	<input type="submit" name="cmd" value="Mul">
 	<input type="submit" name="cmd" value="Div"><br>
-	Result: xxxxxx
+	Result: <?=$res?>
 </form>
 
 </body>

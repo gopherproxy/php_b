9f1a8b2a8e355104e08bd3b4a6e2f74239069a85
Tue Becher <tue@Tues-Mac.local>
2017-03-29 14:31:18 +0200

    Initial newsletter assignment

---
+++ newsletter.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+
+/*
+1) SignUp -> gem email og dato/tid i db
+- oprette en "newsletter" tabel i en database
+- lav SQL til at indsætte testdata
+- ny php fil "newsletter.php" med formular
+- in php: get email parameter
+- connect to db
+- form prepared statement
+- bind email param
+- execute
+- test!!!
+
+2) Husk serverside validation
+3) Deploy til webhost
+4) SignOut -> slet email fra db
+5) Deploy til webhost
+6) Lav side der viser emails fra db i html tabel (sorter nyeste først)
+7) Deploy til webhost
+8) Implement clientside validation  
+9) Deploy til webhost
+*/
+
+
+
+
+
+
+</body>
+</html>
